c61d5381aee0defc2cb3b12c7297f710ab101abd
MD JABER HUSSAIN <mdjaberofficial@gmail.com>
2026-02-20 00:13:56 +0600

    delete feature implemented with sweet alert

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router';
+import CoffeeCard from './CoffeeCard';
 
 const Home = () => {
-    const coffees = useLoaderData();
+    const loadedCoffees = useLoaderData();
+    const [coffees, setCoffees] = useState(loadedCoffees);
     console.log(coffees);
     return (
         <div>
-            
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                {
+                    coffees.map(coffee => <CoffeeCard 
+                        key={coffee._id} 
+                        coffee={coffee}
+                        coffees={coffees}
+                        setCoffees={setCoffees}
+                        ></CoffeeCard>)
+                }
+            </div>
         </div>
     );
 };
